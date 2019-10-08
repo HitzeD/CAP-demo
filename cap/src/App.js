a70cda3040496444097c6ebb17d9dcfb12c9b5b4
HitzeD <d-hitze@comcast.net>
2019-10-08 12:07:35 -0400

    basic wireframe deployed

--- cap/src/App.js
+++ cap/src/App.js
@@ -3,8 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-		<h1>Custom Alloy Products</h1>
+	<div className="App">
+	
+		<h1>Welcome!</h1>
+
+		<div className="cis-main">
+			<img src="https://images.squarespace-cdn.com/content/v1/5c54259751f4d4405ce0351a/1553701418127-KIB7TBQUFJ6U0P37OP3U/ke17ZwdGBToddI8pDm48kLenUilbe05ffZhuy9kOuzcUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKctTDDtspV9LlWCR4iDVmsoGRaFngo3QcXniEfa_t8QuCp_mYkylZnXuNrdyQ1_Ewo/CIS_homepageLogos-07.png?format=750w" alt="" />
+		</div>
+		
+		<div className="sisters">
+
+			<img src="https://images.squarespace-cdn.com/content/v1/5c54259751f4d4405ce0351a/1553701350533-DNFQZTJ313ILZANXT7UA/ke17ZwdGBToddI8pDm48kLenUilbe05ffZhuy9kOuzcUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKctTDDtspV9LlWCR4iDVmsoGRaFngo3QcXniEfa_t8QuCp_mYkylZnXuNrdyQ1_Ewo/CIS_homepageLogos-08.png?format=750w" alt="" />
+
+			<img src="https://images.squarespace-cdn.com/content/v1/5c54259751f4d4405ce0351a/1553701380794-XX4CILD3NBNJNFT6RW49/ke17ZwdGBToddI8pDm48kFBJ4xyt-HeGjCzNknVx5yoUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcYGs3jGfjIcrjalcOABWtsi2pU6Qmv1l61TrwXXovFfQht1NTSVnpDqLVzYVAY203/CIS_homepageLogos-09.png?format=750w" alt="" />
+
+		</div>
+
     </div>
   );
 }

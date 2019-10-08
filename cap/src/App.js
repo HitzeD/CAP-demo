a2630d0c1c4ef1cbc77ec7b800aabf2f6524105c
HitzeD <d-hitze@comcast.net>
2019-10-08 12:15:57 -0400

    finished with basic page

--- cap/src/App.js
+++ cap/src/App.js
@@ -5,20 +5,37 @@ function App() {
   return (
 	<div className="App">
 	
-		<h1>Welcome!</h1>
-
 		<div className="cis-main">
 			<img src="https://images.squarespace-cdn.com/content/v1/5c54259751f4d4405ce0351a/1553701418127-KIB7TBQUFJ6U0P37OP3U/ke17ZwdGBToddI8pDm48kLenUilbe05ffZhuy9kOuzcUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKctTDDtspV9LlWCR4iDVmsoGRaFngo3QcXniEfa_t8QuCp_mYkylZnXuNrdyQ1_Ewo/CIS_homepageLogos-07.png?format=750w" alt="" />
 		</div>
 		
 		<div className="sisters">
+			<div className="indiana">
+			
+				<img src="https://images.squarespace-cdn.com/content/v1/5c54259751f4d4405ce0351a/1553701350533-DNFQZTJ313ILZANXT7UA/ke17ZwdGBToddI8pDm48kLenUilbe05ffZhuy9kOuzcUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKctTDDtspV9LlWCR4iDVmsoGRaFngo3QcXniEfa_t8QuCp_mYkylZnXuNrdyQ1_Ewo/CIS_homepageLogos-08.png?format=750w" alt="" />
 
-			<img src="https://images.squarespace-cdn.com/content/v1/5c54259751f4d4405ce0351a/1553701350533-DNFQZTJ313ILZANXT7UA/ke17ZwdGBToddI8pDm48kLenUilbe05ffZhuy9kOuzcUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKctTDDtspV9LlWCR4iDVmsoGRaFngo3QcXniEfa_t8QuCp_mYkylZnXuNrdyQ1_Ewo/CIS_homepageLogos-08.png?format=750w" alt="" />
+				<div>
+					<h3>9701 Old State Road 25</h3>
+					<h3>Lafayette, IN 47905</h3>
+					<h3>(765) 564-4684</h3>
+				</div>
 
-			<img src="https://images.squarespace-cdn.com/content/v1/5c54259751f4d4405ce0351a/1553701380794-XX4CILD3NBNJNFT6RW49/ke17ZwdGBToddI8pDm48kFBJ4xyt-HeGjCzNknVx5yoUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcYGs3jGfjIcrjalcOABWtsi2pU6Qmv1l61TrwXXovFfQht1NTSVnpDqLVzYVAY203/CIS_homepageLogos-09.png?format=750w" alt="" />
+			</div>
+			
+			<div className="texas">
 
+				<img src="https://images.squarespace-cdn.com/content/v1/5c54259751f4d4405ce0351a/1553701380794-XX4CILD3NBNJNFT6RW49/ke17ZwdGBToddI8pDm48kFBJ4xyt-HeGjCzNknVx5yoUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcYGs3jGfjIcrjalcOABWtsi2pU6Qmv1l61TrwXXovFfQht1NTSVnpDqLVzYVAY203/CIS_homepageLogos-09.png?format=750w" alt="" />
+
+				<div>
+					<h3>25918 Hardin Store Road</h3>
+					<h3>Magnolia, TX 77354</h3>
+					<h3>(281) 470-2512</h3>
+				</div>
+			</div>
+			
 		</div>
-
+		<h1>I appreciate your time!</h1>
+		
     </div>
   );
 }
